0941cfd08e00b5e5478f8bd01a6947c8d3425c96
Hataroux <hatari@hatarax.com>
2018-10-23 09:46:30 +0200

    Ajout du fragment d'architecture

--- app/src/main/java/com/example/vmisu/projet/Architecture.kt
+++ app/src/main/java/com/example/vmisu/projet/Architecture.kt
@@ -5,14 +5,40 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class Architecture : Fragment() {
+class Architecture : Fragment(), AnkoLogger {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cours, container, false)
+        return inflater.inflate(R.layout.fragment_architecture, container, false)
+        info("ON CREATE")
     }
 
+    override fun onStart() {
+        super.onStart()
+        info("ON START")
+    }
 
+    override fun onResume() {
+        super.onResume()
+        info("ON RESUME")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        info("ON PAUSE")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        info("ON STOP")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        info("ON DESTROY")
+    }
 }
